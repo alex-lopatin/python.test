9326ce8cf8bc9697021991bddb8fc41724f15a32
alex-lopatin <mail.lopatin@gmail.com>
2016-12-21 11:22:47 +0300

    маленький тестовый скриптик

---
+++ raznotest.py
@@ -0,0 +1,23 @@
+
+def hi(K):
+    print("grblsdfbg, " + K + "?")
+
+
+hi("a1")
+hi("b2")
+hi("c3")
+
+def procent(x, y):
+    """qwerty"""
+    one_per = x / 100
+    res = y / one_per
+    print(str(y) + " is " + str(res) + " прОцентов of " + str(x))
+    return res
+
+
+def prunt_percent(x,y):
+    """ddldllf"""
+    print(str(y) + " is " + str(procent(x, y)) + " прОцентов of " + str(x))
+
+procent(300,87)
+print (prunt_percent)
